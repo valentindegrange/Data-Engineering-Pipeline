23c02c086b1e799b9f3204a018f2be7c1e0933e0
Valentin Degrange <valentin.degrange@gmail.com>
2026-02-02 22:43:11 +0100

    Improve extraction function

--- scripts/extract.py
+++ scripts/extract.py
@@ -1,3 +1,4 @@
+from typing import List
 # this is my script extract file
 
 def foo():
@@ -6,3 +7,18 @@ def foo():
 
 def bar():
   print('bar')
+
+
+def extract(data: List) -> List:
+  new_data = list()
+  keys = ['id', 'count', 'amount']
+  
+  for item in data:
+    cropped_item = dict()
+    for key, value in item.keys():
+      if key in keys:
+        cropped_item[key] = value
+    new_data.append(cropped_item)
+
+  return new_data 
+        
