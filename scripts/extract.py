c0b26285fb4f941ac41248b944df433b3b4c3817
Valentin Degrange <valentin.degrange@gmail.com>
2026-02-04 22:18:43 +0100

    Allow extract to filter data

--- scripts/extract.py
+++ scripts/extract.py
@@ -6,3 +6,11 @@ def foo():
 
 def bar():
   print('bar')
+
+
+def extract(data: dict):
+  keys = ['id', 'count', 'amount']
+  new_data = dict()
+  for k in keys:
+    new_data[k] = data[k]
+  return new_data
